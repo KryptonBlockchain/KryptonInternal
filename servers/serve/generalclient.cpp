aa1e4405f92eb29f93a3befb18ccb0eb1625174b
xss <58095671+CipherVector@users.noreply.github.com>
2021-08-06 01:47:39 +0800

    Hmm here too
    :100: :+1: :+1:

--- servers/serve/generalclient.cpp
+++ servers/serve/generalclient.cpp
@@ -6,6 +6,16 @@
 #include <atomic>
 #include <iostream>
 
+#include <openssl/aes.h>
+#include <openssl/evp.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
+
+#include <assert.h>
+
 class ChatClient : public LinkKing::Asio::TCPClient
 {
 public:
